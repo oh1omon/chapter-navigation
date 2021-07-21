ea6ab9f6b2c29ae8483ad9d35532ced870592a1d
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-21 22:29:04 +0300

    feat: installed and setted up styled-components to work

---
+++ src/assets/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+	* {
+	  	margin: 0;
+	  	padding: 0;
+	  	outline: 0;
+	  	box-sizing: border-box;
+	  	font-family: 'Poppins', sans-serif;
+	}
+	
+	button {
+	  font: inherit
+	}
+`
