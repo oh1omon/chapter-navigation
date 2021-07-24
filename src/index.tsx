f88c7cae85ae9731747eecade7419d347cc6a441
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-24 13:29:12 +0300

    feat: tied parameters passed to TableOfContent components to url pathname

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { GlobalStyles } from './assets/styles/GlobalStyles'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<GlobalStyles />
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
