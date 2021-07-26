427de6632947a444c0cf54ab49bbbe700e504eb7
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-26 18:25:55 +0300

    docs: added docs for search component

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -5,13 +5,19 @@ import { SLink } from './TableOfContents'
 const Search = () => {
 	const text = useSelector((store: IRootStore) => store.text)
 
+	// Local state for result object
 	const [result, setResult] = useState<TText>({})
 
+	/**
+	 * This handler is run every time user changes input
+	 * Algorithm will filter only first 10 results with the word or phrase user has searched for
+	 * @param e
+	 */
 	const searchHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		setResult(
 			Object.keys(text)
 				.filter((i) => text[i].includes(e.target.value))
-				.slice(0, 5)
+				.slice(0, 10)
 				.reduce((acc, curr) => {
 					acc[curr] = text[curr]
 					return acc
@@ -23,6 +29,7 @@ const Search = () => {
 			<label htmlFor={'search'} />
 			<input id={'search'} onChange={(e) => searchHandler(e)} />
 			<div>
+				{/*Displaying results as SLinks, so user could fastly navigate to the chapter and result*/}
 				{Object.keys(result).map((i) => (
 					<SLink to={`${i}`} selected={false} key={i}>
 						{i} {text[i]}
