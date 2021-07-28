543cfbd57cb9f91a3722a7e1bb480d6ec2f62865
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-28 17:46:10 +0300

    docs: updated comments for Breadcrumbs comp

--- src/Components/Breadcrumbs.tsx
+++ src/Components/Breadcrumbs.tsx
@@ -2,54 +2,48 @@ import React from 'react'
 import { IBreadcrumbsProps } from '../react-app-env'
 import { SLink } from './TableOfContents'
 
-const Breadcrumbs = ({ rule }: IBreadcrumbsProps) => {
+const Breadcrumbs = ({ rule }: IBreadcrumbsProps): JSX.Element => {
 	return (
 		<p>
+			{/* We will render breadcrumbs, only if anything has been already selected */}
 			{rule.chapter ? (
 				<>
+					{/* Link to the / */}
 					<SLink to={'/'} selected={false}>
 						{rule.theme}
 					</SLink>
 
+					{/* Link to all chapters of this number */}
 					{rule.chapter > 9 && (
 						<>
 							&gt;
 							<SLink to={`/${rule.theme}`} selected={false}>
+								{`${rule.theme}00`}
+							</SLink>
+						</>
+					)}
+
+					{/*Link to all rule of selected chapter */}
+					{rule.chapter > 9 && `${rule.theme}00` !== String(rule.chapter) && (
+						<>
+							&gt;
+							<SLink to={`/${rule.chapter}`} selected={false}>
 								{rule.chapter}
 							</SLink>
 						</>
 					)}
+
+					{/* Showing user result number */}
 					{typeof rule.result === 'string' && rule.result.length > 4 && (
 						<>
 							&gt;
-							<SLink to={`/${rule.chapter}`} selected={false}>
-								{rule.result}
-							</SLink>
+							{rule.result}
 						</>
 					)}
 				</>
 			) : (
-				' '
+				<span>&nbsp;</span>
 			)}
-			{/*<>*/}
-			{/*	<SLink to={'/'} selected={false}>*/}
-			{/*		{rule.theme}*/}
-			{/*	</SLink>*/}
-			{/*	&gt;*/}
-			{/*</>*/}
-			{/*{rule.chapter > 9 && (*/}
-			{/*	<>*/}
-			{/*		<SLink to={`/${rule.theme}`} selected={false}>*/}
-			{/*			{rule.chapter}*/}
-			{/*		</SLink>*/}
-			{/*		&gt;*/}
-			{/*	</>*/}
-			{/*)}*/}
-			{/*{rule.result && (*/}
-			{/*	<SLink to={`/${rule.chapter}`} selected={false}>*/}
-			{/*		{rule.result}*/}
-			{/*	</SLink>*/}
-			{/*)}*/}
 		</p>
 	)
 }
