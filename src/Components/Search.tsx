a861e537ba07baf694a93cff395067df719e2875
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-30 11:52:42 +0300

    feat: added icon for search input

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -2,6 +2,43 @@ import React, { ChangeEvent, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { SLink } from './TableOfContents'
 import { IRootStore, TText } from '../react-app-env'
+import SearchIcon from '../assets/icons/SearchIcon'
+import styled from 'styled-components'
+
+const SLabel = styled.label`
+	position: relative;
+
+	& > input {
+		color: white;
+		padding: 0.5em;
+		background-color: black;
+		border: 0;
+		border-bottom: 2px solid white;
+		width: 25em;
+	}
+	& > input:focus {
+		background-color: white;
+		color: black;
+	}
+
+	& > svg {
+		position: absolute;
+		right: 0;
+		margin: 0.5em;
+	}
+`
+
+const SDiv = styled.div`
+	width: 17em;
+`
+
+const ResultCellDiv = styled.div`
+	padding-top: 1em;
+	border-bottom: 4px solid white;
+	width: 100%;
+	font-size: 14px;
+	text-align: justify;
+`
 
 const Search = (): JSX.Element => {
 	const text = useSelector((store: IRootStore) => store.text)
@@ -15,10 +52,14 @@ const Search = (): JSX.Element => {
 	 * @param e
 	 */
 	const searchHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		if (e.target.value === '') {
+			setResult({})
+			return
+		}
 		setResult(
 			Object.keys(text)
 				.filter((i) => text[i].includes(e.target.value))
-				.slice(0, 10)
+				.slice(0, 5)
 				.reduce((acc, curr) => {
 					acc[curr] = text[curr]
 					return acc
@@ -27,16 +68,27 @@ const Search = (): JSX.Element => {
 	}
 	return (
 		<>
-			<label htmlFor={'search'} />
-			<input id={'search'} onChange={(e) => searchHandler(e)} />
-			<div>
+			<SLabel htmlFor={'search'}>
+				<input id={'search'} type={'text'} placeholder={'Search'} onChange={(e) => searchHandler(e)} />
+				<SearchIcon />
+			</SLabel>
+
+			<SDiv>
 				{/*Displaying results as SLinks, so user could fastly navigate to the chapter and result*/}
 				{Object.keys(result).map((i) => (
-					<SLink to={`${i}`} key={i}>
-						{i} {`${text[i].split(' ').slice(0, 15).join(' ')}...`}
+					<SLink
+						to={`${i}`}
+						key={i}
+						onClick={() => {
+							setResult({})
+						}}
+					>
+						<ResultCellDiv>
+							{i} {`${text[i].split(' ').slice(0, 15).join(' ')}...`}
+						</ResultCellDiv>
 					</SLink>
 				))}
-			</div>
+			</SDiv>
 		</>
 	)
 }
