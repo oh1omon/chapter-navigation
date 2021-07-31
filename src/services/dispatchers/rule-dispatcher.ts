f0efa76b52e04dc25bf9b44da999c45f6ec18115
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-31 19:33:13 +0300

    feat: added implementations for setting error state on

--- src/services/dispatchers/rule-dispatcher.ts
+++ src/services/dispatchers/rule-dispatcher.ts
@@ -1,18 +1,26 @@
 import { Dispatch } from 'redux'
 import * as actionTypes from '../../store/actions/rule-actions'
+import { SET_ERROR } from '../../store/actions/error-actions'
 
 /**
  * This function is going to set Rule component with data from location pathname passed as param
- * @param location
+ * @param pathname
  */
-export const setRule = (location: string) => {
+export const setRule = (pathname: string) => {
 	return (dispatch: Dispatch): void => {
+		if (pathname.length > 8) {
+			// Dispatching error if the link is absolutely wrong
+			// 8 symbols is the biggest amount of char length of rule number
+			dispatch({ type: SET_ERROR, payload: true })
+			return
+		}
+
 		dispatch({
 			type: actionTypes.SET_RULE,
 			payload: {
-				theme: Number(location.charAt(1)),
-				chapter: Number(location.slice(1, 4)) || 0,
-				result: location.length > 3 && location.slice(1),
+				theme: Number(pathname.charAt(1)),
+				chapter: Number(pathname.slice(1, 4)) || 0,
+				result: pathname.length > 3 && pathname.slice(1),
 			},
 		})
 	}
