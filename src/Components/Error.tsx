bb74978fb9ac437ec49a292a3d9cf609e8397399
Artem Zagarov <artem.zagarov@gmail.com>
2021-07-31 19:54:56 +0300

    feat: finalized work with 404

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import coolestErrImg from '../assets/images/404.jpg'
 
-const Error = () => {
-	return <div></div>
+const SContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+
+	& > img :before {
+		content: 'mmmmm';
+		position: absolute;
+		white-space: pre;
+		display: inline;
+		top: 0;
+		left: 50%;
+		transform: translate(-50%, -25px);
+		z-index: 30;
+	}
+`
+
+const Error = (): JSX.Element => {
+	return (
+		<SContainer>
+			<img src={coolestErrImg} alt={'404, the wrong url'} />
+		</SContainer>
+	)
 }
 
 export default Error
